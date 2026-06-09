3442d504e891f1adcc5250ec1c6c4a8ed2468bd0
Daren Loxley <2e0lxy.daren@gmail.com>
2026-06-09 19:05:10 +0100

    fix: add asStateFlow import

--- app/src/main/java/uk/aprsnet/client/AprsViewModel.kt
+++ app/src/main/java/uk/aprsnet/client/AprsViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import uk.aprsnet.client.aprs.PacketParser
