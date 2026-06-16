b999347801b9117df316b06833c563d8fb31b40e
Daren Loxley <2e0lxy.daren@gmail.com>
2026-06-16 09:47:23 +0100

    chore: bump to 2.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "uk.aprsnet.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 41
-        versionName = "2.7.0"
+        versionCode = 42
+        versionName = "2.7.1"
     }
 
     buildTypes {
