50d813eb3a3c5f32b7fd56c23af89623a4bfd600
Daren Loxley <2e0lxy.daren@gmail.com>
2026-06-27 13:41:39 +0100

    chore: bump 2.9.9→3.0.0 (fix wear compile errors)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "uk.aprsnet.client"
         minSdk = 26
         targetSdk = 35
-        versionCode = 54
-        versionName = "2.9.9"
+        versionCode = 55
+        versionName = "3.0.0"
     }
 
     buildTypes {
